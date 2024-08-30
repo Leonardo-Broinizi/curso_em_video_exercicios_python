a9dbe4641ba4b7c28842e9f5266adfe4a4622ab1
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-08-30 13:10:12 -0300

    .

--- exercicios_mundo_03/#19_dicionarios/ex093_analise_jogador_futebol.py
+++ exercicios_mundo_03/#19_dicionarios/ex093_analise_jogador_futebol.py
@@ -1,14 +1,15 @@
+#    Obs.: Pensei que meu código estaria muito parecido com o do professor, por isso não anotei o dele,
+# mas percebí que o código do professor estava sensívelmente melhor que o meu e fui anotando as diferenças.
+
 jogador = {}
 partidas = []
-tot = 0
 jogador['nome'] = str(input('\033[1mNome do jogador: ')).strip()
 npartidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
 for c in range(npartidas):
-    partidas.append(int(input(f'Quantos gols na partida {c}? ')))
-    tot += partidas[c]
-jogador['gols'] = partidas
-jogador['total'] = tot
-print('-=' * 40 + '-')
+    partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+jogador['gols'] = partidas[:] # Eu me lembrei, vendo o vídeo do professor, de colocar [:] para não gerar uma ligação entre as listas. Embora nos meus testes a lista de dentro do dicionário não tenha ficado com a ligação com a de fora, é bom não me esquecer disso!
+jogador['total'] = sum(partidas) # Eu tinha feito uma variável variável acumuladora chamada 'tot' para calcular o total de gols mas ví, pelo código do professor, que com o método 'sum' fica bem mais prático.
+print('-=' * 40 + '-') # Aqui o professor fez um for, economizando várias linhas, da seguinte maneira: for k, v in jogador.items(): (e na linha de baixo:  print(f'O campo {k} tem o valor {v}')
 print(f'Jogador:  {jogador}')
 print('-=' * 40 + '-')
 print(f'O campo nome tem o valor {jogador['nome']}.')
@@ -17,5 +18,5 @@ print(f'O campo total tem o valor {jogador['total']}.')
 print('-=' * 40 + '-')
 print(f'O jogador {jogador['nome']} jogou {npartidas} partidas.')
 for c in range(npartidas):
-    print(f'    => Na partida {c}, fez {jogador['gols'][c]} gols.')
+    print(f'    => Na partida {c+1}, fez {jogador['gols'][c]} gols.')
 print(f'Foi um total de {jogador['total']} gols.')
--- exercicios_mundo_03/#19_dicionarios/ex094_cadastro_dicionarios_dentro_de_listas.py
+++ exercicios_mundo_03/#19_dicionarios/ex094_cadastro_dicionarios_dentro_de_listas.py
@@ -1,4 +1,6 @@
-lista_cadastros = []
+#    Meu código:
+
+'''lista_cadastros = []
 cadastro = {}
 cópia = {}
 média = 0
@@ -6,14 +8,23 @@ print('\033[1m','-=' * 35 + '-')
 while True:
     cont = ' '
     cadastro['nome'] = str(input('Nome: ')).strip()
-    cadastro['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+    while True:
+        cadastro['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+        if cadastro['sexo'] in 'MF':
+            break
+        else:
+            print('ERRO! Por favor, responda apenas S ou M.')
     cadastro['idade'] = int(input('Idade: '))
     cópia = cadastro.copy()
     lista_cadastros.append(cópia)
     cadastro.clear()
     média += cópia['idade']
-    while cont not in 'SN':
+    while True:
         cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if cont not in 'SN':
+            print('ERRO! Por favor, responda apenas S ou N.')
+        else:
+            break
     if cont == 'N':
         break
 média /= len(lista_cadastros)
@@ -24,8 +35,49 @@ print(f'As mulheres cadastradas foram: ', end='')
 for c in lista_cadastros:
     if c['sexo'] == 'F':
         print(c['nome'], end=' - ')
-print('\nAs pessoas com idade acima da média são: ', end=' ')
+print('\nAs pessoas com idade acima da média são: ')
 for c in lista_cadastros:
     if c['idade'] > média:
-        print(f'Nome = {c['nome']}; sexo = {c['sexo']}; idade = {c['idade']};')
-print(f'{'<< ENCERRADO >>':^33}')
+        print(f'    Nome = {c['nome']}; sexo = {c['sexo']}; idade = {c['idade']};')
+print(f'{'<< ENCERRADO >>':^33}')'''
+
+#    Código do professor Guanabara:
+
+galera = list()
+pessoa = dict()
+soma = média = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.') # Interessante notar que, nessa iteração, o uso de 'else' é desnecessário, já que, caso o if não seja satisfeito, a próxima linha será execultada, e, caso seja satisfeito, o break impedirá sua execussão.
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print(f'B) A média de idade é de {média:5.2f} anos.')
+print(f'C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p['nome']}', end=' ')
+print()
+print(f'D) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= média:
+        print('    ', end=' ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end=' ')
+        print()
+print('<< ENCERRADO >>')
