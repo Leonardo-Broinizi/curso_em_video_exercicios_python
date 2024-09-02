8241df11f0369affa83e6e49824de46395854b54
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-09-02 18:02:53 -0300

    .

--- exercicios_mundo_03/#19_dicionarios/ex093_analise_jogador_futebol.py
+++ exercicios_mundo_03/#19_dicionarios/ex093_analise_jogador_futebol.py
@@ -1,7 +1,7 @@
 #    Obs.: Pensei que meu código estaria muito parecido com o do professor, por isso não anotei o dele,
 # mas percebí que o código do professor estava sensívelmente melhor que o meu e fui anotando as diferenças.
 
-jogador = {}
+'''jogador = {}
 partidas = []
 jogador['nome'] = str(input('\033[1mNome do jogador: ')).strip()
 npartidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
@@ -19,4 +19,72 @@ print('-=' * 40 + '-')
 print(f'O jogador {jogador['nome']} jogou {npartidas} partidas.')
 for c in range(npartidas):
     print(f'    => Na partida {c+1}, fez {jogador['gols'][c]} gols.')
-print(f'Foi um total de {jogador['total']} gols.')
+print(f'Foi um total de {jogador['total']} gols.')'''
+
+#    Finalmente deixei de preguiça e copiei o código do professor rs:
+
+#    Meu código (não consegui formatar a variável da lista de gols feitos para ocuparem todas o
+# mesmo número de colunas):
+
+'''jogador = {}
+partidas = []
+jogadores = []
+while True:
+    print(f'\033[1m{'-' * 30}')
+    res = ' '
+    tot = 0
+    jogador['nome'] = str(input('Nome do jogador: ')).strip()
+    npartidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+    for c in range(npartidas):
+        partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+        tot += partidas[c]
+    jogador['gols'] = partidas.copy()
+    jogador['total'] = tot
+    jogadores.append(jogador.copy())
+    jogador.clear()
+    partidas.clear()
+    while res not in 'SN':
+        res = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if res == 'N':
+        break
+print('-=' * 20 + '-')
+print('cod nome            gols            total\n',f'{'-' * 40}')
+for p, cad in enumerate(jogadores):
+    print(f'{p+1:>2} {cad['nome']:15} {cad['gols']} {cad['total']:>12}')
+while True:
+    print('-' * 49)
+    while True:
+        res = int(input('  Mostrar dados de qual jogador? [999 para SAIR]: '))
+        if res > len(jogadores) and res != 999 or res == 0:
+            print('\nVALOR INCORRETO! Tente novamente.\n')
+        else:
+            break
+    if res == 999:
+        print('<< VOLTE SEMPRE >>')
+        break
+    else:
+        print(f'-- LEVANTAMENTO DO JOGADOR {jogadores[res-1]['nome']}:')
+        for p, g in enumerate(jogadores[res-1]['gols']):
+            print(f'No jogo {p+1} fez {g} gols.')'''
+
+#    Código do professor Guanabara:
+
+jogador = {}
+partidas = list()
+jogador['nome'] = str(input('Nome do jogador: '))
+tot = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+for c in range(0, tot):
+    partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador['nome']} jogou {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador['gols']):
+    print(f'    => Na partida {i}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
--- exercicios_mundo_03/#19_dicionarios/ex095_analise_de_jogador_ampliado.py
+++ exercicios_mundo_03/#19_dicionarios/ex095_analise_de_jogador_ampliado.py
@@ -1,3 +1,6 @@
+#    Meu código (não consegui formatar a variável da lista de gols feitos para ocuparem todas o
+# mesmo número de colunas):
+
 jogador = {}
 partidas = []
 jogadores = []
@@ -8,7 +11,7 @@ while True:
     jogador['nome'] = str(input('Nome do jogador: ')).strip()
     npartidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
     for c in range(npartidas):
-        partidas.append(int(input(f'Quantos gols na partida {c}? ')))
+        partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
         tot += partidas[c]
     jogador['gols'] = partidas.copy()
     jogador['total'] = tot
@@ -19,22 +22,57 @@ while True:
         res = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if res == 'N':
         break
-print('-=' * 20 + '-')
-print('cod nome            gols            total\n',f'{'-' * 40}')
+print('-=' * 25 + '-')
+print('cod nome            gols                      total')
+print(f'{'-' * 51}')
 for p, cad in enumerate(jogadores):
-    print(f'{p:>2} {cad['nome']:15} {cad['gols']} {cad['total']:>12}')
+    print(f'{p+1:>2} {cad['nome']:15} {str(cad['gols']):<16} {cad['total']:>12}') # Atualizei essa formatação com a solução que vi no código do professor.
 while True:
-    print('-' * 49)
+    print('-' * 51)
     while True:
         res = int(input('  Mostrar dados de qual jogador? [999 para SAIR]: '))
-        if res >= len(jogadores) and res != 999:
-            print('VALOR INCORRETO! Tente novamente.')
+        if res > len(jogadores) and res != 999 or res == 0:
+            print('\nVALOR INCORRETO! Tente novamente.\n')
         else:
             break
     if res == 999:
         print('<< VOLTE SEMPRE >>')
         break
     else:
-        print(f'-- LEVANTAMENTO DO JOGADOR {jogadores[res]['nome']}:')
-        for p, g in enumerate(jogadores[res]['gols']):
-            print(f'No jogo {p} fez {g} gols.')
+        print(f'-- LEVANTAMENTO DO JOGADOR {jogadores[res-1]['nome']}:')
+        for p, g in enumerate(jogadores[res-1]['gols']):
+            print(f'No jogo {p+1} fez {g} gols.')
+
+
+#    Código do professor Guanabara:
+'''time = list()
+jogador = {}
+partidas = list()
+
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+    for c in range(0, tot):
+        partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    partidas.clear()
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        else:
+            print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3}', end=' ')
+    for d in v.values():
+        print(f'{str(d):<15}', end='') # Aqui está a maneira que o professor usou para fazer a formatação que eu não consegui! hehe
+    print()
+print('-' * 40)'''
+
+
