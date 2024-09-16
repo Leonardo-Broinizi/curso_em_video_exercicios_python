8d86ea7fdc61c841ffb581fbb6d6ad964c2c89c3
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-09-16 17:54:27 -0300

    .

--- exercicios_mundo_03/#21_funcoes_parte_dois/ex103_ficha_de_jogador.py
+++ exercicios_mundo_03/#21_funcoes_parte_dois/ex103_ficha_de_jogador.py
@@ -9,7 +9,7 @@ ficha(jogador, gols)'''
 
 # Meu código após ver o funcionamento do programa do professor Guanabara:
 
-def ficha(jogador='<desconhecido>', gols=0):
+'''def ficha(jogador='<desconhecido>', gols=0):
     return f'O jogador {jogador} fez {gols} gols(s) no campeonato.'
 
 print('-' * 33)
@@ -23,8 +23,21 @@ elif len(nome) == 0:
 elif len(num) == 0:
     print(ficha(jogador=nome))
 else:
-    print(ficha(nome,num))
+    print(ficha(nome,num))'''
 
-#    Código do professor Guanabara:
+#    Código do professor Guanabara (ficou mais enxuto do que o meu segundo e fez o programa sempre exibir um número no campo de gols, coisa que o meu não fez):
 
+'''def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
 
+# Programa Principal
+n = str(input('Nome do Jogador: '))
+g = str(input('Número de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)'''
